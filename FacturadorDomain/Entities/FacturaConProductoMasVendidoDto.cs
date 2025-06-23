fc3badd1b15b30c15aa967c7accbbbe9d59ed7fc
Damian Daporta <damiandaporta21@gmail.com>
2025-06-23 02:44:25 -0300

    Se crean SP de SP-facturas-producto-mas-vendido y SP-facturas-con-detalle

---
+++ FacturadorDomain/Entities/FacturaConProductoMasVendidoDto.cs
@@ -0,0 +1,9 @@
+﻿public class FacturaConProductoMasVendidoDto
+{
+    public int Fact_ID { get; set; }
+    public DateTime FechaAlta { get; set; }
+    public int Cli_ID { get; set; }
+    public string Estado { get; set; } = string.Empty;
+    public int? ProductoMasVendidoID { get; set; }
+    public string? ProductoMasVendidoNombre { get; set; }
+}
