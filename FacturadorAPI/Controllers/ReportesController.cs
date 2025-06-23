044e38e7127b06e7f5e034de3c8d4c475b517151
Damian Daporta <damiandaporta21@gmail.com>
2025-06-23 02:30:34 -0300

    Se crean Vistas en SQL y sus Endpoints para poder consumirlos

---
+++ FacturadorAPI/Controllers/ReportesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FacturadorInfrastructure.Context;
+using FacturadorDomain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FacturadorAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReportesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReportesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("facturas-monto-total")]
+        public async Task<ActionResult<IEnumerable<FacturaConMontoTotal>>> GetFacturasConMontoTotal()
+        {
+            var result = await _context.Set<FacturaConMontoTotal>().ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("facturas-cliente")]
+        public async Task<ActionResult<IEnumerable<FacturaConCliente>>> GetFacturasConCliente()
+        {
+            var result = await _context.Set<FacturaConCliente>().ToListAsync();
+            return Ok(result);
+        }
+    }
+}
---
+++ FacturadorDomain/Entities/FacturaConCliente.cs
@@ -0,0 +1,8 @@
+﻿public class FacturaConCliente
+{
+    public int FC_ID { get; set; }
+    public DateTime FechaAlta { get; set; }
+    public string RazonSocial { get; set; } = string.Empty;
+    public string CUIT { get; set; } = string.Empty;
+}
+
